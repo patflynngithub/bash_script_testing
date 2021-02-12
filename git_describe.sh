fecf40f065f394c70639b14cf75e181e1d42ad8c
Patrick Flynn <patflynn@hotmail.com>
2021-02-11 22:17:47 -0800

    fourth commit

--- git_describe.sh
+++ git_describe.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-export BUILD_ID=$(git describe --long --dirty)
+export TEST=$(echo "no space" | sed 's/ /_/g')
+echo $TEST
+export BUILD_ID=$(git describe --long --dirty  | sed 's/ /_/g')
 echo $BUILD_ID
 
